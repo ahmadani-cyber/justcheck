202adc68016286d6e269f844ac26042eb8d13fed
Daniyal <butt.daniyalahmad1992@gmail.com>
2020-05-11 00:45:54 -0400

    checkcommit

---
+++ justchecks/src/justchecks/just.java
@@ -0,0 +1,13 @@
+package justchecks;
+
+public class just {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("just check");
+		// TODO Auto-generated method stub
+
+	}
+
+}
